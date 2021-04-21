dd3b70950bcacde7be8e5918f670bb0d1ff1af7a
Alex Balgavy <alex@balgavy.eu>
2021-04-21 22:56:14 +0200

    Fix battery

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
   /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-  {"🔋 ", "printf '%s%%' $(battery | head -n 1)", 60, 0},
+  {" 🔋 ", "echo $(battery | head -n 1 )%", 60, 0},
   {"", "date '+%b %d (%a) %I:%M%p'", 60, 0},
 };
 
