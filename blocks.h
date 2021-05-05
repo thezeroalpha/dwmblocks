8824e6e677d40a932f8a52c38dd803b63aeb3004
Alex Balgavy <alex@balgavy.eu>
2021-05-05 18:03:14 +0200

    Add CPU block, fix signals

--- blocks.h
+++ blocks.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
   /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
-  {"", "dwmblock-battery", 5, 3},
-  {"☀ ", "dwmblock-brightness", 0, 1},
-  {"", "date '+%b %d (%a) %I:%M%p'", 60, 0},
+  {"", "dwmblock-battery", 5, 1},
+  {"", "dwmblock-cpu", 10, 2 },
+  {"☀ ", "dwmblock-brightness", 0, 3},
+  {"", "date '+%b %d (%a) %I:%M%p'", 60, 4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
