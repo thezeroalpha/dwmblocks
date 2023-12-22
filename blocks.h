92a5ad3c83510d485ba505e3be314567274fe34d
Alex Balgavy <alexander.balgavy@spaceapplications.com>
2023-12-22 23:54:43 +0100

    More blocks

--- blocks.h
+++ blocks.h
@@ -1,10 +1,18 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
   /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+  {"", "dwmblock-emacs-org-clock", 10, 7},
   {"", "dwmblock-battery", 5, 1},
+  {"", "dwmblock-doppler", 0, 10},
   {"", "dwmblock-cpu", 10, 2 },
-  {"☀ ", "dwmblock-brightness", 0, 3},
-  {"", "date '+%b %d (%a) %I:%M%p'", 60, 4},
+  {"", "dwmblock-brightness", 0, 3},
+  {"", "dwmblock-internet", 10, 5},
+  {"", "dwmblock-nettraf", 10, 12},
+  {"", "dwmblock-bluetooth", 10, 8},
+  {"", "dwmblock-volume", 10, 6},
+  {"", "dwmblock-kbselect", 0, 11},
+  {"", "dwmblock-startbutton", 60, 13},
+  {"", "date '+%b %d (%a) %I:%M%p'", 1, 4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
